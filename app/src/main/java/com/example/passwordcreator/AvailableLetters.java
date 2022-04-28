4be7a6e57e5112fcfdba303b9434aea8d090c247
Nicolas Dietermann <nicolas.dietermann@gmx.de>
2022-04-28 21:46:36 +0200

    First final version of the app

---
+++ app/src/main/java/com/example/passwordcreator/AvailableLetters.java
@@ -0,0 +1,7 @@
+package com.example.passwordcreator;
+
+public class AvailableLetters {
+
+
+
+}
--- app/src/main/java/com/example/passwordcreator/MainActivity.java
+++ app/src/main/java/com/example/passwordcreator/MainActivity.java
@@ -3,52 +3,86 @@ package com.example.passwordcreator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String[] a;
-    private static String b = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,-!%&/()=?#+*";
-    private static boolean Kleinbuchstaben, Großbuchstaben, Zahlen, Zeichen;
-    private static int länge;
+    Button button;
+    EditText editText_length, editText_password;
+    CheckBox checkBox_capitals, checkBox_lowercase, checkBox_numbers, checkBox_symbols;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button_password);
+        editText_length = findViewById(R.id.edittext_length);
+        editText_password = findViewById(R.id.edittext_password);
+        checkBox_capitals = findViewById(R.id.checkbox_capital_letters);
+        checkBox_lowercase = findViewById(R.id.checkbox_lower_case_letters);
+        checkBox_numbers = findViewById(R.id.checkbox_numbers);
+        checkBox_symbols = findViewById(R.id.checkbox_symbols);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        button.setOnClickListener(v -> {
+            String s1 = editText_length.getText().toString();
+            int i = Integer.parseInt(s1);
 
-    public static void main(String[] args) {
+            String s2 = getCharacters(checkBox_capitals.isChecked(), checkBox_lowercase.isChecked(), checkBox_numbers.isChecked(), checkBox_symbols.isChecked());
+            editText_password.setText(recRandom(i,null,s2));
+        });
+    }
 
-        Kleinbuchstaben = true; // 1 - 26
-        Großbuchstaben = true;  // 27 - 52
-        Zahlen = true;          // 51 - 62
-        Zeichen = true;         // 63 - 76
-        länge = 10;             // Passwortlänge
-        a = new String[76];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = String.valueOf(b.charAt(i));
+    private String getCharacters(boolean capitals, boolean lowercase, boolean numbers, boolean symbols){
+        String s1 = "", s2, s3, s4;
+        if (capitals) {
+            s2 = s1.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        } else {
+            s2 = s1;
         }
-        recRandom(länge, null, 62);
-
+        if (lowercase) {
+            s3 = s2.concat("abcdefghijklmnopqrstuvwxyz");
+        } else {
+            s3 = s2;
+        }
+        if (numbers) {
+            s4 = s3.concat("1234567890");
+        } else {
+            s4 = s3;
+        }
+        if (symbols) {
+            return s4.concat(".,-!%&/()=?#+*");
+        }
+        return s4;
     }
 
-    private static String recRandom(int restbuchstaben, String passwort, int length) {
+    private static String recRandom(int leftCharacters, String password, String availableCharacters) {
 
-        if (restbuchstaben > 0) {
-            restbuchstaben--;
+        if (password == null){
+            // password initialisation
+            String newPassword = "";
+            return recRandom(leftCharacters, newPassword, availableCharacters);
+
+        } else if (leftCharacters > 0) {
+            /* Recursive Part.
+             * This part adds an random character every time it is called.
+             */
+            leftCharacters--;
             Random random = new Random();
-            int zahl = random.nextInt(length);
-            if (passwort == null) {
-                String newPasswort = a[zahl];
-                return recRandom(restbuchstaben, newPasswort, length);
-            } else {
-                String newPasswort = passwort + a[zahl];
-                return recRandom(restbuchstaben, newPasswort, length);
-            }
+            int number = random.nextInt(availableCharacters.length());
+            String newPassword = password + availableCharacters.charAt(number);
+            return recRandom(leftCharacters, newPassword, availableCharacters);
+
         } else
-            return passwort;
+            return password;
     }
 
 }
